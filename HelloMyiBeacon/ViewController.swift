16352449b97d5bc6a5ccade9bc7e870f4ab437b5
Mrosstro <ttosam01@gmail.com>
2018-11-22 10:15:04 +0800

    Initial Commit

---
+++ HelloMyiBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMyiBeacon
+//
+//  Created by Mrosstro on 2018/11/22.
+//  Copyright © 2018 Mrosstro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
